98ab3dadaaa487f54f42af4b2768899aa65c6020
Maryna Hapon <marynahapon@gmail.com>
2017-08-26 20:15:04 +0300

    menu, slogan, carousel
    - module 'slogan'
    - components 'menu', 'carousel'
    - base carousel  styles
    - base slogan styles
    - slogan content in JSON file
    - menu fix for IE

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,3 +1,10 @@
     // menu
 var elm = document.querySelector('#menu');
 var ms = new MenuSpy(elm);
+
+
+$('.slogan-carousel').flickity({
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true
+});
