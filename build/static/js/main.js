3c4bad7223b8c0fdb73f6646bdd300470c4bea39
Maryna Hapon <marynahapon@gmail.com>
2017-08-27 22:49:20 +0300

    component 'menu'
    - for mobile devices

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,20 +1,28 @@
     // menu
-var elm = document.querySelector('#menu');
+var elm = document.querySelector("#menu");
 var ms = new MenuSpy(elm);
 
-
-$('.slogan-carousel').flickity({
-    cellAlign: 'center',
+    // carousel
+$(".slogan-carousel").flickity({
+    cellAlign: "center",
     contain: true,
     wrapAround: true
 });
 
-
     // sticky header
-$(window).on('scroll', function() {
+$(window).on("scroll", function() {
     if ($(this).scrollTop() > 0) {
-        $('#menu-top').addClass("navbar-fixed-top");
+        $("#menu-top").addClass("navbar-fixed-top");
     } else {
-        $('#menu-top').removeClass("navbar-fixed-top");
+        $("#menu-top").removeClass("navbar-fixed-top");
     }
+});
+
+    // burger
+$( ".burger" ).click(function() {
+    $( ".navbar" ).toggle( "fast" );
+});
+
+$( ".navbar" ).click(function() {
+    $( ".navbar" ).toggle( "slow" );
 });
