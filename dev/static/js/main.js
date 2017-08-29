e488ceb85dd9ae633879d773619d86b458e4a29d
Maryna Hapon <marynahapon@gmail.com>
2017-08-29 11:13:56 +0300

    component 'navigation'
    - breakpoint: 768px
    - problems with state transitions

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,33 +1,36 @@
 $(document).ready(function() {
 
     // menu
-var elm = document.querySelector("#menu");
-var ms = new MenuSpy(elm);
+    var elm = document.querySelector("#menu");
+    var ms = new MenuSpy(elm);
 
     // carousel
-$(".slogan-carousel").flickity({
-    cellAlign: "center",
-    contain: true,
-    wrapAround: true
-});
+    $(".slogan-carousel").flickity({
+        cellAlign: "center",
+        contain: true,
+        wrapAround: true
+    });
 
     // sticky header
-$(window).on("scroll", function() {
-    if ($(this).scrollTop() > 0) {
-        $("#menu-top").addClass("navbar-fixed-top");
-    } else {
-        $("#menu-top").removeClass("navbar-fixed-top");
-    }
-});
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 0) {
+            $("#menu-top").addClass("navbar-fixed-top");
+        } else {
+            $("#menu-top").removeClass("navbar-fixed-top");
+        }
+    });
 
     // burger
-$( ".burger" ).click(function() {
-    $( ".navbar" ).toggle( "fast" );
-});
 
-$( ".navbar" ).click(function() {
-    $( ".navbar" ).toggle( "slow" );
-});
+    $(".burger").click(function () {
+        $(".navbar").toggle("fast");
+    });
+
+    $(".navbar").click(function () {
+        $(".navbar").toggle("slow");
+    });
+} else {}
+
 
 
     // tabs
